f6d58aa89a508fbdf2b37adbe6fbcee28f64b225
Ravijeet Sharma <ravijeeeetsharma4444@gmail.com>
2024-11-24 07:40:10 +0530

    :wrench: Added utilities to be used across the app

---
+++ src/constants/quotes.js
@@ -0,0 +1,71 @@
+// Create a new file for quotes
+export const motivationalQuotes = [
+  {
+    text: "Small progress is still progress",
+    author: "Unknown",
+  },
+  {
+    text: "Done is better than perfect",
+    author: "Sheryl Sandberg",
+  },
+  {
+    text: "Focus on progress, not perfection",
+    author: "Unknown",
+  },
+  {
+    text: "The secret of getting ahead is getting started",
+    author: "Mark Twain",
+  },
+  {
+    text: "Productivity is never an accident",
+    author: "Peter Drucker",
+  },
+  {
+    text: "The way to get started is to quit talking and begin doing",
+    author: "Walt Disney",
+  },
+  {
+    text: "Don't count the days, make the days count",
+    author: "Muhammad Ali",
+  },
+  {
+    text: "Your daily choices become your destiny",
+    author: "Unknown",
+  },
+  {
+    text: "Success is built one task at a time",
+    author: "Unknown",
+  },
+  {
+    text: "The only way to do great work is to love what you do",
+    author: "Steve Jobs",
+  },
+  {
+    text: "Start where you are, use what you have, do what you can",
+    author: "Arthur Ashe",
+  },
+  {
+    text: "Every accomplished task is a stepping stone towards greatness",
+    author: "Unknown",
+  },
+  {
+    text: "Organization is the key to unlocking your potential",
+    author: "Unknown",
+  },
+  {
+    text: "Little by little, a little becomes a lot",
+    author: "Tanzanian Proverb",
+  },
+  {
+    text: "The difference between try and triumph is just a little umph",
+    author: "Marvin Phillips",
+  },
+  {
+    text: "Your future is created by what you do today",
+    author: "Unknown",
+  },
+  {
+    text: "Take it one task at a time, one day at a time",
+    author: "Unknown",
+  },
+];
---
+++ src/constants/routes.js
@@ -0,0 +1,9 @@
+export const ROUTES = {
+  HOME: "/",
+  LOGIN: "/login",
+  SIGNUP: "/signup",
+  FORGOT_PASSWORD: "/forgot-password",
+  RESET_PASSWORD: "/reset-password",
+  CREATE: "/create",
+  EDIT: "/edit/:id",
+};
